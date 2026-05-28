f6b41459f4a7ba5b06fdd0e06d722b35ba022242
MUSABAJ <musabajebar@gmail.com>
2026-05-28 12:15:53 +0300

    fix: correct typo in sendFile method for production route

--- backend/src/index.js
+++ backend/src/index.js
@@ -47,11 +47,10 @@ app.get('/', (req, res) => {
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-app.get("*",(req, res) => {
-  res.sendFIle(path.join(__dirname,'../frontend', 'dist','index.html'))
-})
+  app.get("*",(req, res) => {
+    res.sendFile(path.join(__dirname,'../frontend', 'dist','index.html'))
+  })
 }
-
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
